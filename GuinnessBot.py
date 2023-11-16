51dfa6746b1d467140e4590ae962799a83379794
GuinnessShep <108033341+GuinnessShep@users.noreply.github.com>
2023-11-15 21:47:05 -0800

    Update GuinnessBot.py

--- GuinnessBot.py
+++ GuinnessBot.py
@@ -239,7 +239,6 @@ if __name__ == "__main__":
        `'                    GUINNESS'
                                '-"-' 
                                 '-  """
-    )
 
     print(
         Colorate.Vertical(
